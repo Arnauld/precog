443bdd2a2e4e2eed95711e884388942e9e3f7e37
Arnauld Loyer <arnauld.loyer@gmail.com>
2012-11-21 09:53:27 +0100

    Add @Before equivalent ~10min

--- src/test/scala/precog/store/DiskStoreSpec.scala
+++ src/test/scala/precog/store/DiskStoreSpec.scala
@@ -5,6 +5,7 @@ import annotation.tailrec
 import precog.util.{Bytes, BytesComparator}
 import java.util.concurrent.atomic.AtomicLong
 import scala.Array
+import org.specs2.specification.Scope
 
 /**
  * 
@@ -15,26 +16,30 @@ class DiskStoreSpec extends Specification {
   implicit def bytes(s:String) = s.getBytes("utf-8")
 
   "DiskStore" should {
-    "be sure that Duplicate values cannot be stored more than once" in {
+    "be sure that Duplicate values cannot be stored more than once" in new basicStoreScope {
       /*
       That is, if you add 10000 keys all with the value "John Doe",
       then the text "John Doe" must be stored only a single time.
       */
-      val valueStore = new BasicBinaryStore()
-      val indexStorage = new BasicIndexStore()
-
-      val store = new DiskStoreAdapter(valueStore, indexStorage)
-
       store.put("1", "John Doe")
       store.put("2", "John Doe")
       store.put("3", "John Doe")
       store.put("4", "John Doe")
       store.put("5", "John Doe")
 
-      valueStore.addresses.toList must contain(Bytes("John Doe") -> Address(Some(0))).only
+      val soleEntry = (Bytes("John Doe") -> Address(Some(0)))
+
+      valueStore.addresses.toList must contain(soleEntry).only
 
     }
 
+  }
+
+  trait basicStoreScope extends Scope {
+    lazy val valueStore = new BasicBinaryStore()
+    lazy val indexStorage = new BasicIndexStore()
+
+    lazy val store = new DiskStoreAdapter(valueStore, indexStorage)
   }
 }
 
